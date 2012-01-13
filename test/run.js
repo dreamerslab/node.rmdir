1dc007ec15cd945a1a3293bc9f5ad52a96a0aa12
ben <ben@dreamerslab.com>
2012-01-13 23:28:53 +0800

    add test

--- test/run.js
+++ test/run.js
@@ -1,24 +1,4 @@
-var packer = require('../index'),
-    path   = __dirname + '/assets/';
+var rmdirr = require( '../lib/rmdirr' ),
+    path = __dirname + '/assets';
 
-packer({
-  log: true,
-  type: 'js',
-  input: [
-    path + 'dojo.js',
-    path + 'jquery.js',
-    path + 'prototype.js'
-  ],
-  output: path + 'pack.min.js'
-});
-
-packer({
-  log: true,
-  minify: true,
-  type: 'css',
-  input: [
-    path + 'reset.css',
-    path + 'reset-html5.css'
-  ],
-  output: path + 'pack.min.css'
-});
+rmdirr( path );

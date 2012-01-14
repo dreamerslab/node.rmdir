f9451dbb1df3c4fff1e4d461f08c239ca074813b
ben <ben@dreamerslab.com>
2012-01-14 11:16:36 +0800

    fix typo

--- test/run.js
+++ test/run.js
@@ -1,7 +1,7 @@
 var rmdir = require( '../lib/rmdirr' ),
     path  = __dirname + '/assets';
 
-rmdirr( __dirname + '/assets', function ( err, dirs, files ){
+rmdir( __dirname + '/assets', function ( err, dirs, files ){
   console.log( dirs );
   console.log( files );
   console.log( 'all files are removed' );

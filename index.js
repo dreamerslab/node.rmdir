5e7e7568f4e9f39c79ea08130543baa1639d9f0f
jkroso <jkroso@gmail.com>
2013-04-26 13:37:16 +1200

    fix potential issue with `cb` being called more than once

--- index.js
+++ index.js
@@ -11,21 +11,22 @@ var fs = require('fs')
 
 module.exports = function rmdir(path, cb) {
   fs.readdir(path, function(e, files) {
-    if (e) return cb && cb(e);
+    if (e) return done(e);
     var i = files.length;
     if (!i) return fs.rmdir(path, cb);
 
     files.forEach(function(file){
       file = join(path, file);
       fs.stat(file, function (e, stat) {
-        if (e) return cb && cb(e);
+        if (e) done(e);
         if (stat.isDirectory()) rmdir(file, done);
         else fs.unlink(file, done);
       });
     });
 
     function done(e){
-      if (e) cb && (e);
+      // `cb` mustn't be called twice
+      if (e) cb && (cb(e), cb = null);
       else if (--i <= 0) fs.rmdir(path, cb);
     }
   });

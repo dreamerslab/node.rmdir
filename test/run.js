08fdddbc59b9d428b2ed9003691ce797846a8adc
ben <ben@dreamerslab.com>
2014-01-09 08:52:19 -0800

    add line breaks in logger

--- test/run.js
+++ test/run.js
@@ -3,7 +3,7 @@ var path  = __dirname + '/assets';
 
 rmdir( __dirname + '/assets', function ( err, dirs, files ){
   if( err ) return console.log( 'err', err );
-  console.log( 'dirs', dirs );
-  console.log( 'files', files );
+  console.log( 'dirs : \n', dirs );
+  console.log( 'files : \n', files );
   console.log( 'all files are removed' );
 });

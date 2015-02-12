056805c7b2d1f15e6ca1cf1a33188f700042059a
David Pate <davidtpate@gmail.com>
2015-02-12 13:22:09 -0500

    Added the ability to pass in an optional alternative file system to remove the files from and associated tests/docs

---
+++ test/runFs.js
@@ -0,0 +1,9 @@
+var rmdir = require( '../lib/rmdir' );
+var path  = __dirname + '/assets';
+
+rmdir( __dirname + '/assets', { fs: require('fs') }, function ( err, dirs, files ){
+  if( err ) return console.log( 'err', err );
+  console.log( 'dirs : \n', dirs );
+  console.log( 'files : \n', files );
+  console.log( 'all files are removed' );
+});

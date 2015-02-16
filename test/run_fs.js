35894a81118634abeef167514be361d47094c327
ben <ben@dreamerslab.com>
2015-02-16 17:45:05 +0800

    syntax tuning

--- test/run_fs.js
+++ test/run_fs.js
@@ -1,7 +1,7 @@
 var rmdir = require( '../lib/rmdir' );
 var path  = __dirname + '/assets';
 
-rmdir( __dirname + '/assets', { fs: require('fs') }, function ( err, dirs, files ){
+rmdir( __dirname + '/assets', { fs: require( 'fs' )}, function ( err, dirs, files ){
   if( err ) return console.log( 'err', err );
   console.log( 'dirs : \n', dirs );
   console.log( 'files : \n', files );
